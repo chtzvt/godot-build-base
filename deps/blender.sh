9263466e416927553e1e6d98a68852733e12876f
Charlton Trezevant <ct@ctis.me>
2023-12-26 00:38:03 -0500

    additional blender dependencies

--- deps/blender.sh
+++ deps/blender.sh
@@ -3,3 +3,5 @@ curl -o blender.tar.xz https://cdn.ctis.me/file/ctisme-cdn/files-pub/ext/water-m
 tar xf blender.tar.xz && mv -v $(ls -d */ | grep blender) /base/blender
 
 rm -f blender.tar.xz
+
+apt -y install libsm6 libxext6 libxrender-dev libgl1-mesa-dev libglu1-mesa-dev
